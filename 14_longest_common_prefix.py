93b864c36bd0f04e751705ee72f8df7ccfd609ee
Namsraijav <namsraijav0415@gmail.com>
2024-07-28 13:20:49 +0800

    longest common prefix solved

--- 14_longest_common_prefix.py
+++ 14_longest_common_prefix.py
@@ -25,7 +25,35 @@
 
 class Solution(object):
     def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
+        ret = strs[0]
+        for i in range(1, len(strs)):
+            str1 = strs[i-1]
+            str2 = strs[i]
+            if i == 1:
+                ret = getMatchedString(str1, str2)
+            elif ret == "":
+                return ret
+            else:
+                ret = getMatchedString(ret, str2)
+        return ret
+
+
+
+def getMatchedString(str1, str2):
+    min = len(str1)
+    addstr = ""
+    if(min > len(str2)):
+        min = len(str2)
+    for i in range(min):
+        if (str1[i] == str2[i]):
+            addstr += str1[i]
+        else:
+            return addstr
+    return addstr
+
+
+
+
+
+Solution().longestCommonPrefix(["flower","flow","flight"])
+
