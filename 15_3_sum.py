fcc10653d954e7deebee61a3981c5a497b6660b9
Namsraijav <namsraijav0415@gmail.com>
2024-08-01 09:49:37 +0800

    time limit exceeded 15

--- 15_3_sum.py
+++ 15_3_sum.py
@@ -43,34 +43,36 @@ class Solution(object):
                 for k in range(j+1, len(nums)):
                     if (nums[i] + nums[j] + nums[k] == 0):
                         arr.append([nums[i], nums[j], nums[k]])
-        print("arr1", arr)
-        print("cleared", clear_duplicate(arr))
         return clear_duplicate(arr)
 
 def clear_duplicate(nums):
     arr = []
-    if len(nums) >= 1:
-        arr.append(nums[0])
-    for i in range(1, len(nums) - 1):
-        # print("i", i)
-        for j in range(len(nums)):
-            print("asasd",nums[i], nums[j])
-            if is_dupl(nums[i], nums[j]) == False and i != j:
-                arr.append(nums[i])
+    for i in range(len(nums)):
+        check = False
+        for j in range(i + 1, len(nums)):
+            if is_dupl(nums[i], nums[j]):
+                check = True
+        if not check:
+            arr.append(nums[i])
     return arr
 
 def is_dupl(nums1, nums2):
-    count = 0
-    for num1 in nums1:
-        for num2 in nums2:
-            if num1 == num2:
-                count += 1
-    # print("count",count)
-    return count >= 3
+    idx1 = []
+    idx2 = []
+    for i in range(len(nums1)):
+        num1 = nums1[i]
+        for j in range(len(nums2)):
+            num2 = nums2[j]
+            if num1 == num2 and not has_contain(i, idx1) and not has_contain(j, idx2):
+                idx1.append(i)
+                idx2.append(j)
 
+    return len(idx1) == 3
 
+def has_contain(idx ,arr):
+    ret = False
+    for num in arr:
+        ret = num == idx
+    return ret
 
-
-Solution().threeSum([-2,0,1,1,2]
-
-)
+Solution().threeSum([-13,5,13,12,-2,-11,-1,12,-3,0,-3,-7,-7,-5,-3,-15,-2,14,14,13,6,-11,-11,5,-15,-14,5,-5,-2,0,3,-8,-10,-7,11,-5,-10,-5,-7,-6,2,5,3,2,7,7,3,-10,-2,2,-12,-11,-1,14,10,-9,-15,-8,-7,-9,7,3,-2,5,11,-13,-15,8,-3,-7,-12,7,5,-2,-6,-3,-10,4,2,-5,14,-3,-1,-10,-3,-14,-4,-3,-7,-4,3,8,14,9,-2,10,11,-10,-4,-15,-9,-1,-1,3,4,1,8,1])
