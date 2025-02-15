e5c5e5e81b6f5d746d9bf9212c234ecd9ef2acf3
Namsraijav <namsraijav0415@gmail.com>
2025-02-15 16:34:36 +0800

    time limit exceeded

--- 92_sort_array.py
+++ 92_sort_array.py
@@ -1,9 +1,20 @@
+
+from typing import List
+
 class Solution(object):
-    def sortArray(self, nums):
+    def sortArray(self, nums: List[int]) -> List[int]:
         """
         :type nums: List[int]
         :rtype: List[int]
         """
+        current = []
+        while len(nums) != 0:
+            temp = nums.pop()
+            while len(current) > 0 and current[len(current) -1] > temp:
+                nums.append(current.pop())
+
+            current.append(temp)
+        return current
 
 
 
