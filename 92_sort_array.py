f6d1f64c21e980385e7e38522e04a882150a1ea3
Namsraijav <namsraijav0415@gmail.com>
2025-02-15 16:27:11 +0800

    sort array

---
+++ 92_sort_array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def sortArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+
+
+
+solution = Solution()
+print(solution.sortArray([1,3,5,2,4]))
