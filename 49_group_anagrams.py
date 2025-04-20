65ae63c77c5e6e02cbd477cbfe07759b2b9eac31
namsrai-dev <namsrai.dev@gmail.com>
2025-04-20 18:18:50 +0800

    group of anagram

---
+++ 49_group_anagrams.py
@@ -0,0 +1,11 @@
+def group_anagrams(strings):
+    anagram_groups = {}
+    for string in strings:
+        canonical = ''.join(sorted(string))
+        if canonical in anagram_groups:
+            anagram_groups[canonical].append(string)
+        else:
+            anagram_groups[canonical] = [string]
+    return list(anagram_groups.values())
+
+print(group_anagrams(["hhhhu","tttti","tttit","hhhuh","hhuhh","tittt"]))
