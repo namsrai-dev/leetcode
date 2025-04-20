3b4e687a4c4809eb5af6adb431496803472b30fe
namsrai-dev <namsrai.dev@gmail.com>
2025-04-20 18:58:26 +0800

    two sum done

---
+++ Arrays and hashing/two_sum.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums) - 1):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
+        
+
+solution = Solution()
+print(solution.twoSum([2,7,11,15], 9))
