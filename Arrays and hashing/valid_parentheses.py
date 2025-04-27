3e214c8534ef3c31e2a4915c810dbfb0eb818b5f
namsrai-dev <namsrai.dev@gmail.com>
2025-04-27 14:32:38 +0800

    valid parentheses done

---
+++ Arrays and hashing/valid_parentheses.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        arr = []
+        for i in s:
+            if i in "([{":
+                arr.append(i)
+            else:
+                if len(arr) < 1:
+                    return False
+                popped = arr.pop()
+                if i == ")":
+                    if popped != "(":
+                        return False
+                elif i == "}":
+                    if popped != "{":
+                        return False
+                else:
+                    if popped != "[":
+                        return False
+        if len(arr) != 0:
+            return False
+        return True
+                
+        
+
+sol = Solution()
+print(sol.isValid("([[]])"))
