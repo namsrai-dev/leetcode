aa5c363da1816af64758fc20663a09f4dc150834
namsrai-dev <namsrai.dev@gmail.com>
2025-05-26 08:02:32 +0800

    max profit done

---
+++ sliding_window/best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        higher, lower = prices[0], prices[0]
+        max_val = 0
+        for i in range(1, len(prices)):
+            if lower > prices[i]:
+                higher, lower = prices[i], prices[i]
+            else:
+                higher = prices[i]
+                max_val = max(max_val, higher - lower)
+        return max_val
+
+
+sol = Solution()
+print(sol.maxProfit([7,6,4,3,1]))
