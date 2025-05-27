b644841ca8fd0217cf4af3fb72e51d0ce71511e4
namsrai-dev <namsrai.dev@gmail.com>
2025-05-27 09:31:58 +0800

    longest substring without repeating

---
+++ sliding_window/longest_substring_without_repeating.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        window_arr = []
+        ret = 0
+        for i in s:
+            if i not in window_arr:
+                window_arr.append(i)
+            else:
+                while i in window_arr:
+                    window_arr.pop(0)
+                window_arr.append(i)
+            ret = max(ret, len(window_arr))
+        return ret
+
+
+
+sol = Solution()
+print(sol.lengthOfLongestSubstring("aab"))
