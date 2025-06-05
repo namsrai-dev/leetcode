e6485a1b529539df4766271323f5fd8b8c995c43
namsrai-dev <namsrai.dev@gmail.com>
2025-06-05 10:23:23 +0800

    iterative binary search done

--- binary search/binary_search.py
+++ binary search/binary_search.py
@@ -1,28 +1,28 @@
 class Solution:
     def search(self, nums: list[int], target: int) -> int:
-        is_found = False
-        l, r = 0, len(nums)-1
-        num_index = (r - l) // 2 + l 
-        
-        while is_found is not True:
-            if nums[num_index] == target:
-                return num_index
-            elif l == r-1:
-                return -1
-            elif nums[num_index] < target:
-                l = num_index
-            else:
-                r = num_index
-            num_index = (r - l) // 2 + l 
+        l, r = 0, len(nums)
 
+        while l < r:
+            m = l + ((r - l) // 2)  
+            if nums[m] >= target:
+                r = m
+            elif nums[m] < target:
+                l = m + 1
+        return l if (l < len(nums) and nums[l] == target) else -1
+
+# 7 // 2 => 3
+
+# 5 < 13
+
+# l = 5
+# r = 6 
 
 
 
 
 sol = Solution()
-print(sol.search([-1,0,3,5,9,12,13], 15))
+# print(sol.search([-1,0,3,5,9,12,13], 13))
+print(sol.search([-1,0,3,5,9,12,13], 2))
+# print(sol.search([5], -5))
+# print(sol.search([2,5], 2))
 
-#  6 // 2 => 3
-#  7 // 2 => 3  
-
-# 3, 7 // 2   ->   (7 - 3) / 2 + 3 
