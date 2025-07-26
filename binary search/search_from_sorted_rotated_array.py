000bf576c1ad5df99e36032f1b5ff3322aacd9aa
namsrai-dev <namsrai.dev@gmail.com>
2025-07-26 14:01:43 +0800

    solved binary sorted array

--- binary search/search_from_sorted_rotated_array.py
+++ binary search/search_from_sorted_rotated_array.py
@@ -3,17 +3,33 @@ class Solution:
         l = 0
         r = len(nums)-1
         while r >= l:
-            mid = (r - l) // 2
-            print(mid, l, r)
-            if mid == target:
+            mid = (r + l) // 2
+            # print(mid, l, r)
+            # print("case", nums[l] < nums[mid] and target >= nums[l] and target < nums[mid])
+            if nums[mid] == target:
                 return mid
-            elif l < mid and target >= l and target <= r:
-                r = mid - 1
+            elif nums[l] < nums[mid]: # when order is l to mid
+                if target >= nums[l] and target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            elif nums[mid] < nums[r]: # when order is mid to r
+                if target > nums[mid] and target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
             else:
-                l = mid + 1
+                print("gajig", l, mid, r)
+                if l == r:
+                    return -1
+                else:
+                    l = mid + 1
         return -1
 
         
 
 sol = Solution()
-print(sol.search([4,5,6,7,0,1,2], 9))
+print(sol.search([4,5,6,7,0,1,2], 3))
+print(sol.search([3,1], 0))
+# print(sol.search([5,6,7,0,1,2,3,4], 9))
+# print(sol.search([5,6,7,3,4], 6))
