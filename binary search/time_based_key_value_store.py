e0a75e95ad2de40582aacd42f2abbb06dadcf7f7
namsrai-dev <namsrai.dev@gmail.com>
2025-07-29 09:21:33 +0800

    time based key value store

--- binary search/time_based_key_value_store.py
+++ binary search/time_based_key_value_store.py
@@ -10,10 +10,28 @@ class TimeMap:
 
     def get(self, key: str, timestamp: int) -> str:
         res = ""
-        values = self.store[key].get(key, [])
+        values = self.store.get(key, [])
+        l, r = 0, len(values) -1
+        while l <= r:
+            m = (l+r) // 2
+            if values[m][1] <= timestamp:
+                res = values[m][0]
+                l = m + 1
+            else:
+                r = m - 1
+
+        return res
 
 
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
 # obj.set(key,value,timestamp)
 # param_2 = obj.get(key,timestamp)
+
+timeMap = TimeMap()
+timeMap.set("foo", "bar", 1);  # store the key "foo" and value "bar" along with timestamp = 1.
+timeMap.get("foo", 1);         # return "bar"
+timeMap.get("foo", 3);         # return "bar", since there is no value corresponding to foo at timestamp 3 and timestamp 2, then the only value is at timestamp 1 is "bar".
+timeMap.set("foo", "bar2", 4); # store the key "foo" and value "bar2" along with timestamp = 4.
+timeMap.get("foo", 4);         # return "bar2"
+timeMap.get("foo", 5);         # return "bar2"
