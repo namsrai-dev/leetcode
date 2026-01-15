458f78db7b1a45e807705fb19310fdbb2f479321
namsrai-dev <namsrai.dev@gmail.com>
2026-01-15 09:38:53 +0800

    add methods

--- linked list/reverse_nodes_in_k_group.py
+++ linked list/reverse_nodes_in_k_group.py
@@ -9,10 +9,38 @@ class LinkedList:
         self.head = None
 
     def append(self, val):
+        new_node = ListNode(val)
         if not self.head:
-            self.head = ListNode(val)
+            self.head = new_node
         else:
             temp = self.head
-            while temp:
+            while temp.next:
                 temp = temp.next
-                
+
+            temp.next = new_node
+
+    def print_list(self):
+        temp = self.head
+        while temp:
+            print("new_node -> ", temp.val)
+            temp = temp.next
+
+    
+    def merge_group(self, k):
+        temp = self.head
+        while temp:
+            temp = self.group_list(self, temp, k)
+
+    def group_list(self, head):
+
+        return None
+
+
+my_list = LinkedList()
+my_list.append(1)
+my_list.append(2)
+my_list.append(3)
+my_list.append(4)
+my_list.append(5)
+
+my_list.print_list()
