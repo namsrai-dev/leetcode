3a6bb59a022abed248a849d863876f7fe324a798
namsrai-dev <namsrai.dev@gmail.com>
2026-01-16 09:12:00 +0800

    reverse in group

--- linked list/reverse_nodes_in_k_group.py
+++ linked list/reverse_nodes_in_k_group.py
@@ -29,11 +29,25 @@ class LinkedList:
     def merge_group(self, k):
         temp = self.head
         while temp:
-            temp = self.group_list(self, temp, k)
+            tail = self.group_list(temp, k)
+            temp = tail.next if tail else None
 
-    def group_list(self, head):
+    def group_list(self, head, k):
+        cnt = 1
+        temp = head
+        while temp and cnt < k:
+            cnt += 1
+            temp = temp.next
 
-        return None
+        print("head value =>", head.val)
+        print("tail value =>", temp.val if temp else None)
+        if cnt == k:
+            self.revese_list(k)
+
+    def revese_list(self, k):
+        pass
+
+        # return temp
 
 
 my_list = LinkedList()
@@ -42,5 +56,8 @@ my_list.append(2)
 my_list.append(3)
 my_list.append(4)
 my_list.append(5)
+my_list.append(6)
 
 my_list.print_list()
+
+my_list.merge_group(2)
