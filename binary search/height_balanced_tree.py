7d39d4842b8baafabb1eaaa58f826ed556160eec
namsrai-dev <namsrai.dev@gmail.com>
2026-01-29 09:42:19 +0800

    prettier

--- binary search/height_balanced_tree.py
+++ binary search/height_balanced_tree.py
@@ -14,7 +14,7 @@ class Solution:
 
             left, right = dfs(root.left), dfs(root.right)
 
-            balanced = (left[0] and right[0] and abs(left[1] - right[1]) <=1 )
+            balanced = (left[0] and right[0] and abs(left[1] - right[1]) <=1)
 
             return [balanced, 1 + max(left[1], right[1])]
 
