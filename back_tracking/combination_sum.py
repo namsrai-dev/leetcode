463615ef64b2251fb929eaf3bbb841c2c47eb54d
namsrai-dev <namsrai.dev@gmail.com>
2026-04-10 16:39:11 +0800

    combination sum backtrack done

--- back_tracking/combination_sum.py
+++ back_tracking/combination_sum.py
@@ -47,19 +47,32 @@ from typing import List
 class Solution:
     def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
         ret = []
-        arr = []
 
-        def dfs(arr):
-            pass
+        def arr_sum(my_arr):
+            total_sum = 0
+            for i in my_arr:
+                total_sum += i
+            return total_sum
 
+        def dfs(arr1, arr2):
+            print()
+            if arr_sum(arr2) == target:
+                ret.append(arr2)
+                # return
+            
+            for i, num in enumerate(arr1):
+                new_arr = arr2.copy()
+                new_arr.append(num)
+                if arr_sum(new_arr) <= target:
+                    dfs(arr1[i:], new_arr)
 
         for i, num in enumerate(nums):
-            # dfs(i)
-            print(i, num)
+            dfs(nums[i:], [num])
+            # print(i, num)
 
         return ret
 
 
 sol = Solution()
 
-print(sol.combinationSum([2,5,6,9], 9))
+print(sol.combinationSum([3,4,5], 16))
