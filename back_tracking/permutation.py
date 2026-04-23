936ba4cedba4d381bc5ff51c9030249eb6f34203
namsrai-dev <namsrai.dev@gmail.com>
2026-04-23 09:13:20 +0800

    subset 2 first try

--- back_tracking/permutation.py
+++ back_tracking/permutation.py
@@ -29,16 +29,16 @@ class Solution:
             # print("ret -- ")
             return [[]]
 
-        # print("recursing still", nums)
+        print("recursing still", nums)
         perms = self.permute(nums[1:])
-        # print("perms", perms)
+        print("perms", perms)
         res = []
         for p in perms:
             for i in range(len(p) + 1):
                 p_copy = p.copy()
                 p_copy.insert(i, nums[0])
                 res.append(p_copy)
-        # print("res in first", res)
+        print("res in first", res)
         return res
     
 sol = Solution()
