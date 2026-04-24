8e83bc6d53200be2706e49c893903c183d5c4478
namsrai-dev <namsrai.dev@gmail.com>
2026-04-24 10:15:11 +0800

    my solution could not check one edge case

--- back_tracking/word_search.py
+++ back_tracking/word_search.py
@@ -45,19 +45,86 @@
 from typing import List
 
 
+# class Solution:
+#     def exist(self, board: List[List[str]], word: str) -> bool:
+#         ret = False
+
+#         def backtrack(idx_i, idx_j, count, letter, dir=None):
+#             nonlocal ret
+#             if letter == word:
+#                 # print("word found !!!")
+#                 ret = True
+#                 return
+#             elif count == len(word):
+#                 # print("word could not found !!!", letter)
+#                 return
+            
+#             # go right if possible
+#             if idx_j + 1 < len(board[idx_i]) and dir != "left":
+#                 backtrack(idx_i, idx_j+1, count+1, letter+board[idx_i][idx_j+1], "right")
+
+#             # go left if possible
+#             if idx_j - 1 >= 0 and dir != "right":
+#                 # print("check", idx_i, idx_j)
+#                 backtrack(idx_i, idx_j-1, count+1, letter+board[idx_i][idx_j-1], "left")
+
+#             # go up if possible
+#             if idx_i - 1 >= 0 and dir != "down":
+#                 backtrack(idx_i - 1, idx_j, count+1, letter+board[idx_i-1][idx_j], "up")
+
+#             # # go down if possible
+#             if idx_i + 1 < len(board) and dir != "down":
+#                 # print("len board",len(board))
+#                 backtrack(idx_i + 1, idx_j, count+1, letter+board[idx_i+1][idx_j], "up")
+
+
+
+
+#         for idx_i, i  in enumerate(board):
+#             for idx_j, j  in enumerate(i):
+#                 # print()
+#                 backtrack(idx_i, idx_j, 1, j)
+
+#         return ret
+
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        pass
+        ROWS, COLS = len(board), len(board[0])
+        visited = [[False for _ in range(COLS)] for _ in range(ROWS)]
+
+        def dfs(r, c, i):
+            if i == len(word):
+                return True
+            if (r < 0 or c < 0 or r >= ROWS or c >= COLS or
+                word[i] != board[r][c] or visited[r][c]):
+                return False
+
+            visited[r][c] = True
+            res = (dfs(r + 1, c, i + 1) or
+                   dfs(r - 1, c, i + 1) or
+                   dfs(r, c + 1, i + 1) or
+                   dfs(r, c - 1, i + 1))
+            visited[r][c] = False
+            return res
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if dfs(r, c, 0):
+                    return True
+        return False
 
 
 sol = Solution()
 
-board = [
-  ["A","B","C","D"],
-  ["S","A","A","T"],
-  ["A","C","A","E"]
-],
+# board = [
+#   ["A","B","C","D"],
+#   ["S","A","A","T"],
+#   ["A","C","A","E"]
+# ]
 
-word = "CAT"
+# word = "CAT"
 
-sol.exist(board, word)
+board=[["a","a","a","a"],["a","a","a","a"],["a","a","a","a"]]
+word="aaaaaaaaaaaaa"
+
+print(sol.exist(board, word))
