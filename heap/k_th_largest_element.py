e0a7903b10f6fe7ef6c7a6a09770f97fa7e89c4e
namsrai-dev <namsrai.dev@gmail.com>
2026-04-28 09:22:19 +0800

    last stone weight

--- heap/k_th_largest_element.py
+++ heap/k_th_largest_element.py
@@ -33,14 +33,19 @@
 # There will always be at least k integers in the stream when you search for the kth integer.
 
 
+import heapq
 from typing import List
-
 
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
-        pass
-        
+        self.minHeap, self.k = nums, k
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
 
     def add(self, val: int) -> int:
-        pass
+        heapq.heappush(self.minHeap, val)
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
