f5b721439f268ac5f13ba8435412324836f27500
namsrai-dev <namsrai.dev@gmail.com>
2026-08-29 10:31:21 +0800

    group anagram solved

---
+++ Arrays and hashing/group_anagram_test.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        res = defaultdict(list)
+        print(res.values)
+        for s in strs:
+            sortedS = ''.join(sorted(s))
+            res[sortedS].append(s)
+        return list(res.values())
+
+sol = Solution()
+print(sol.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
