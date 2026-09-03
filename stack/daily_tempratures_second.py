e07c74b402e20a8d3238f9927238d6412d530cff
namsrai-dev <namsrai.dev@gmail.com>
2026-09-03 11:58:16 +0800

    my own try

---
+++ stack/daily_tempratures_second.py
@@ -0,0 +1,15 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: list[int]) -> list[int]:
+        # res = [0] * len(temperatures)
+        # stack = []  # pair: [temp, index]
+
+        # for i, t in enumerate(temperatures):
+        #     while stack and t > stack[-1][0]:
+        #         stackT, stackInd = stack.pop()
+        #         res[stackInd] = i - stackInd
+        #     stack.append((t, i))
+        # return res
+        pass
+    
+sol = Solution()
+print(sol.dailyTemperatures([30,38,30,36,35,40,28]))
