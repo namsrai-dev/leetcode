64a593afa2052e83997dcc7179bb981c93b7991d
namsrai-dev <namsrai.dev@gmail.com>
2026-09-09 11:28:09 +0800

    car fleet done

--- stack/car_fleet_second.py
+++ stack/car_fleet_second.py
@@ -1,40 +1,43 @@
 from typing import List
 
 
+# class Solution:
+#     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+#         new_position = []
+#         fleet = 0
+#         for i in range(len(position)):
+#             new_position.append([position[i], speed[i]])
+#         new_position.sort(key=lambda x: x[0], reverse=True)
+#         prev_time = 0
+#         for idx, i in enumerate(new_position):
+#             print(idx, i)
+#             if idx == 0:
+#                 prev_time = (target - i[0]) / i[1]
+#             else:
+#                 curr_time = (target - i[0]) / i[1]
+#                 print(curr_time, prev_time)
+#                 if curr_time <= prev_time:
+#                     fleet+= 1
+#                 # else:
+#                 #     prev_time = curr_time
+
+            
+#         print(new_position)
+       
+#         return len(position) - fleet
+
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        new_position = []
-        fleet = 0
-        for i in range(len(position)):
-            new_position.append([position[i], speed[i]])
-        new_position.sort(key=lambda x: x[0], reverse=True)
-        print(new_position)
-        i = 0
-        while i < target:
-            for idx, num in enumerate(new_position):
-                if not new_position[idx][0] >= target:
-                    pos = new_position[idx][0]
-                    speed = new_position[idx][1]
-                    if idx == 0:
-                        new_position[idx][0] = pos + speed
-                    elif pos + speed >= new_position[idx-1][0] and new_position[idx][0] != new_position[idx-1][0]:
-                        print("fleet",idx, num)
-                        fleet += 1
-                        new_position[idx][0] = new_position[idx-1][0]
-                        new_position[idx][1] = new_position[idx-1][1]
-                    else:
-                        new_position[idx][0] = pos + speed
-
-            i+=1
-        print("new_position ->",new_position)
-
-        # speed -> [2,2,1,1]
-        
-        # 0 -> 1 -> 2 -> 3
-        # 1 -> 3 -> 5 -> 7
-        # 4 -> 6 -> 8 -> 10
-        # 7 -> 8 -> 9 -> 10
-        return fleet
+        pair = [(p, s) for p, s in zip(position, speed)]
+        pair.sort(reverse=True)
+        stack = []
+        print(pair)
+        for p, s in pair:  # Reverse Sorted Order
+            print(stack)
+            stack.append((target - p) / s)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
 
 sol = Solution()
 print(sol.carFleet(10, [1,4], [3,2]))
