b2469ecb3dcc0ef87f93dd05e9e6abc797785fdb
namsrai-dev <namsrai.dev@gmail.com>
2026-09-09 13:13:01 +0800

    car fleet another option

--- stack/car_fleet_second.py
+++ stack/car_fleet_second.py
@@ -1,43 +1,39 @@
 from typing import List
 
 
-# class Solution:
-#     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-#         new_position = []
-#         fleet = 0
-#         for i in range(len(position)):
-#             new_position.append([position[i], speed[i]])
-#         new_position.sort(key=lambda x: x[0], reverse=True)
-#         prev_time = 0
-#         for idx, i in enumerate(new_position):
-#             print(idx, i)
-#             if idx == 0:
-#                 prev_time = (target - i[0]) / i[1]
-#             else:
-#                 curr_time = (target - i[0]) / i[1]
-#                 print(curr_time, prev_time)
-#                 if curr_time <= prev_time:
-#                     fleet+= 1
-#                 # else:
-#                 #     prev_time = curr_time
-
-            
-#         print(new_position)
-       
-#         return len(position) - fleet
-
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        pair = [(p, s) for p, s in zip(position, speed)]
-        pair.sort(reverse=True)
-        stack = []
-        print(pair)
-        for p, s in pair:  # Reverse Sorted Order
-            print(stack)
-            stack.append((target - p) / s)
-            if len(stack) >= 2 and stack[-1] <= stack[-2]:
-                stack.pop()
-        return len(stack)
+        new_position = []
+        fleet = 1
+        for i in range(len(position)):
+            new_position.append([position[i], speed[i]])
+        new_position.sort(key=lambda x: x[0], reverse=True)
+        prev_time = 0
+        for idx, i in enumerate(new_position):
+            print(idx, i)
+            if idx == 0:
+                prev_time = (target - i[0]) / i[1]
+            else:
+                curr_time = (target - i[0]) / i[1]
+                print(curr_time, prev_time)
+                if curr_time > prev_time:
+                    fleet+= 1
+                    prev_time = curr_time
+
+        return fleet
+
+# class Solution:
+#     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+#         pair = [(p, s) for p, s in zip(position, speed)]
+#         pair.sort(reverse=True)
+#         stack = []
+#         print(pair)
+#         for p, s in pair:  # Reverse Sorted Order
+#             print(stack)
+#             stack.append((target - p) / s)
+#             if len(stack) >= 2 and stack[-1] <= stack[-2]:
+#                 stack.pop()
+#         return len(stack)
 
 sol = Solution()
 print(sol.carFleet(10, [1,4], [3,2]))
